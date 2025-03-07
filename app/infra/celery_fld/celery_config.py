d51d442abf169376bae0d4c528e7c0e1bce556b6
Ural Kozhokmatov <ural@Urals-MacBook-Pro.local>
2025-03-07 22:23:07 +0600

    celery configs to env

--- app/infra/celery_fld/celery_config.py
+++ app/infra/celery_fld/celery_config.py
@@ -1,9 +1,15 @@
 from celery import Celery
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 celery_app = Celery(
     "tasks",
-    broker="redis://localhost:6377/0", 
-    backend="redis://localhost:6377/0",
+    # broker="redis://localhost:6377/0", 
+    # backend="redis://localhost:6377/0",
+    broker = os.getenv("CELERY_BROKER_URL"),
+    backend = os.getenv("CELERY_RESULT_BACKEND"),
     include=["app.infra.tasks.email_tasks", "app.infra.tasks.vid_tasks"]
 )
 
