5673700f1571f13103065b20bbd5bfec15a72dbd
Ural Kozhokmatov <ural@Urals-MacBook-Pro.local>
2025-03-16 12:22:36 +0600

    added cache for entity

--- app/api/routes/entiities.py
+++ app/api/routes/entiities.py
@@ -9,7 +9,8 @@ from app.domain.dtos.entity import EntityCreateDTO, EntityUpdateDTO
 from app.domain.entities.entity import Entity
 from app.domain.security.auth_token import decode_access_token
 from app.domain.security.auth_user import user_authorization
-
+from app.infra.redis_fld.redis_config import redis_client
+import asyncio, json, os
 
 router = APIRouter(    
     prefix='/api/ent',
@@ -19,7 +20,7 @@ token_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token")
 
 
 @router.put("/update", response_model=dict)
-def update_entity(
+async def update_entity(
     entity_data: EntityUpdateDTO,
     token: str = Depends(token_scheme),
     db: Session = Depends(get_db)
@@ -35,7 +36,8 @@ def update_entity(
     if not entity:
         raise HTTPException(status_code=404, detail="Вы не создали профайл!")
     entity_service.update_entity(entity, entity_data)
-    
+    cache_key = f"entity:{user.id}"
+    await redis_client.delete(cache_key)
     return {"message": "Entity updated successfully", "updated_fields": entity_data.model_dump(exclude_none=True)}
 
 
@@ -57,19 +59,23 @@ def create_entity(
 
 
 @router.get("/{user_id}", response_model=Entity)
-def get_user_entity(user_id: int, db: Session = Depends(get_db)):
-    """Fetch the entity of the currently logged-in user."""
-    
+async def get_user_entity(user_id: int, db: Session = Depends(get_db)):
+    """Fetch the entity of the currently logged-in user (with Redis caching)."""
+    cache_key = f"entity:{user_id}"
+    cached_results = await redis_client.get(cache_key)
+    if cached_results:
+        return Entity.model_validate(json.loads(cached_results))
     entity_service = EntityService(db)
     entity = entity_service.get_entity_by_user_id(user_id)
-
     if not entity:
         raise HTTPException(status_code=404, detail="No entity found")
-
-    return entity
+    entity_pydantic = Entity.model_validate(entity)
+    entity_json = entity_pydantic.model_dump(mode="json")
+    await redis_client.setex(cache_key, 1800, json.dumps(entity_json))
+    return entity_pydantic 
 
 @router.delete("/{entity_id}")
-def delete_entity(
+async def delete_entity(
     entity_id: int,
     token: str = Depends(token_scheme),
     db: Session = Depends(get_db)
@@ -79,5 +85,6 @@ def delete_entity(
     """
     user = user_authorization(token, db)
     entity_service = EntityService(db)
-    
+    cache_key = f"entity:{user.id}"
+    await redis_client.delete(cache_key)
     return entity_service.delete_entity(user, entity_id)
--- app/domain/entities/entity.py
+++ app/domain/entities/entity.py
@@ -12,4 +12,5 @@ class Entity(BaseModel):
     entity_whatsapp: str
     created_at: datetime
     
-    model_config = ConfigDict(from_attributes=True)
+    class Config:
+        from_attributes = True
